7ec5aaf690e8a20e031d5501b71b49022a80b220
Trevbo25 <trevorbrown25@gmail.com>
2018-04-10 11:25:37 -0700

    made a home component to link to other apps

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -2,9 +2,30 @@ import React from 'react';
 import {Link} from 'react-router-dom';
 
 const Home = () => {
+  const styles = {
+    button: {
+      "backgroundColor": "black",
+      "color": "white",
+      "border": "none",
+      "margin": "10px",
+      "fontSize": "20px",
+      "padding": "10px",
+      "cursor": "pointer",
+      "borderRadius": "3px"
+    },
+    title: {
+
+    }
+  }
   return (
     <div>
-      
+      <h1 style={styles.title}>JAVASCRIPT 5 MINI</h1>
+      <div>
+        <Link to="/app1"><button style={styles.button}>APP1</button></Link>
+        <Link to="/app2"><button style={styles.button}>APP2</button></Link>
+        <Link to="/app3"><button style={styles.button}>APP3</button></Link>
+        <Link to="/app4"><button style={styles.button}>APP4</button></Link>
+      </div>
     </div>
   )
 }
